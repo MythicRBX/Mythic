c53175d85ffd08bdd8fba2040fd75bda2ad384aa
Mythic <155851138+MythicRBX@users.noreply.github.com>
2024-03-23 23:23:34 -0300

    .

---
+++ Utils/KeyPress.lua
@@ -0,0 +1,16 @@
+local VirtualInputManager = game:GetService('VirtualInputManager')
+
+Input = {
+	Apertar = function(Tecla)
+		VirtualInputManager:SendKeyEvent(true, Tecla, false, nil)
+		task.wait(0.005)
+		VirtualInputManager:SendKeyEvent(false, Tecla, false, nil)
+	end,
+	Segurar = function(Tecla, Tempo)
+		VirtualInputManager:SendKeyEvent(true, Tecla, false, nil)
+		task.wait(Tempo)
+		VirtualInputManager:SendKeyEvent(false, Tecla, false, nil)
+	end
+}
+
+return Input
