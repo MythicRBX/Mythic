dd210846b2d2114560e11bc13d1cef853d68d9f0
Mythic <155851138+MythicRBX@users.noreply.github.com>
2024-04-12 13:13:08 -0300

    .

--- Games/RockFruit/RockFruitLoader.lua
+++ Games/RockFruit/RockFruitLoader.lua
@@ -17,6 +17,7 @@ Por Equipe Mythic! :) ❤️
 local Games = {
 	[11448431235] = "https://raw.githubusercontent.com/MythicRBX/Mythic/main/Games/RockFruit/Sea1.lua",
 	[12241341621] = "https://raw.githubusercontent.com/MythicRBX/Mythic/main/Games/RockFruit/Sea2.lua",
+	[16723079713] = "https://raw.githubusercontent.com/MythicRBX/Mythic/main/Games/RockFruit/Raid.lua"
 }
 
 local Link = Games[game.PlaceId]
